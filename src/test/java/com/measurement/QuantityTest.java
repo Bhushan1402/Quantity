44964e86db3eb43bbc33ea7f12d20af3bafa781c
Bhushan <bhushan1402>
2020-04-24 16:40:29 +0530

    [Bhushan]Added:Added Class For Inch and Feet.

--- src/test/java/com/measurement/QuantityTest.java
+++ src/test/java/com/measurement/QuantityTest.java
@@ -11,4 +11,27 @@ public class QuantityTest {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
     }
+
+    @Test
+    public void givenOFeetAnd1Feet_ShouldReturnNotEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1,feet2);
+    }
+
+    @Test
+    public void givenOInchAnd0Inch_ShouldReturnEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(0.0);
+        Assert.assertEquals(inch1,inch2);
+    }
+
+    @Test
+    public void givenOInchAnd1Inch_ShouldReturnNotEqual() {
+        Inch inch1 = new Inch(0.0);
+        Inch inch2 = new Inch(1.0);
+        Assert.assertNotEquals(inch1,inch2);
+    }
+
+
 }
