826243fb18a27229f6f1d240b05f0c8ce27cbac8
Bhushan <bhushan1402>
2020-04-28 01:01:23 +0530

    [Bhushan]Added:Perform Test cases for add two length.

--- src/main/java/com/measurement/QuantityMeasurement.java
+++ src/main/java/com/measurement/QuantityMeasurement.java
@@ -4,6 +4,7 @@ public class QuantityMeasurement {
     public static boolean compare(Length length1, Length length2) {
         return length1.equals(length2);
     }
+
     public static Length addLengths(Length length1, Length length2) {
         double totalLength = length1.quantity+length2.quantity;
         Length lengthInInch = new Length(Length.Unit.INCH, totalLength);
